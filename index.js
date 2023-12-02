7ba1ce395df3e61afb9b8aaa63a6bfdfd200eb7c
akashdeep023 <ad3500476@gmail.com>
2023-12-02 22:40:51 +0530

    update file

--- index.js
+++ index.js
@@ -4,29 +4,24 @@ const moment = require("moment");
 const filePath = "./data.json";
 
 const hackCommit = (n) => {
-    while (n == 0) {
-        let date = Math.floor(Math.random() * 7);
-        let week = Math.floor(Math.random() * 55);
-        const DATE = moment()
-            .subtract(1, "y")
-            .add(1, "d")
-            .add(date, "d")
-            .add(week, "w")
-            .format();
-        console.log(date, "--", week);
-        console.log(DATE);
-        const data = {
-            date: DATE,
-        };
-        jsonFile.writeFile(filePath, data, () => {
-            simpleGit()
-                .add([filePath])
-                .commit(DATE, { "--date": DATE });
-        });
-        if (n == 0) {
-            return simpleGit().push();
-        }
-        n--;
-    }
+	if (n === 0) return simpleGit().push();
+	let date = Math.floor(Math.random() * 7);
+	let week = Math.floor(Math.random() * 40) +15;
+	const DATE = moment()
+		.subtract(1, "y")
+		.add(1, "d")
+		.add(date, "d")
+		.add(week, "w")
+		.format();
+	// console.log(date, "--", week);
+	console.log(DATE);
+	const data = {
+		date: DATE,
+	};
+	jsonFile.writeFile(filePath, data, () => {
+		simpleGit()
+			.add([filePath])
+			.commit(DATE, { "--date": DATE }, hackCommit.bind(this, --n));
+	});
 };
-hackCommit(5);
+hackCommit(500);
