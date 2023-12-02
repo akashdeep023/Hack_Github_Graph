6250be321999c28236b55843373195b854b7f944
akashdeep023 <ad3500476@gmail.com>
2023-12-02 19:20:15 +0530

    update file

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 const jsonFile = require("jsonfile");
-const simpleGit = require('simple-git')
+const simpleGit = require("simple-git");
+const moment = require("moment");
 const filePath = "./data.json";
-const DATE = '2023-12-06T00:48:20+05:30'
-console.log(DATE)
-const data = {
-	date: DATE,
+const hackCommit = (n) => {
+    if (0 < n) {
+        let date = Math.floor(Math.random() * 7)
+        let week = Math.floor(Math.random() * 55)
+        console.log(date,'--',week)
+        const DATE = moment().subtract(1, "y").add(2, "d").add(date,'d').add(week,'w').format();
+        const data = {
+            date: DATE,
+        };
+        console.log(DATE);
+        jsonFile.writeFile(filePath, data,
+            () => {
+                simpleGit().add([filePath]).commit(DATE, { "--date": DATE });
+            });  
+        n--;
+    }
 };
-jsonFile.writeFile(filePath, data);
-simpleGit().add([filePath]).commit(DATE,{'--date':DATE}).push()
+hackCommit(10)
